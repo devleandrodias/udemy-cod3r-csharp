a2fdb37dc35cc214900d71b00563328cf803a01c
Leandro <leandrodbdias@gmail.com>
2020-07-04 13:26:51 -0300

    Entendo estrutura do Array

---
+++ CursoCSharp/CursoCSharp/Colecoes/Array.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CursoCSharp.Colecoes
+{
+    public class Array
+    {
+        public static void Executar()
+        {
+            /*
+             * Array é uma estrutura homogênea
+             * Array é uma estrutura estática (Ela não cresce)
+             * Array é uma estrutura indexada apartir do 0
+             */
+            string[] alunos = new string[5];
+            string []pessoas = new string[2]; // Esse também é um padrão válido
+
+            Console.WriteLine(pessoas);
+
+            alunos[0] = "Leandro";
+            alunos[1] = "Thaísa";
+            alunos[2] = "Beatriz";
+            alunos[3] = "Carlos";
+            alunos[4] = "Manuel";
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            double somatorio = 0;
+
+            double[] notas = { 10.0, 3.4, 2.5, 6.2, 6.2, 7.0 };
+
+            // Usando Foreach
+
+            foreach (var nota in notas)
+            {
+                somatorio += nota;
+            }
+
+            // Usando For
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                somatorio += notas[i];
+            }   
+
+            Console.WriteLine("Média das notas: {0}", somatorio / notas.Length);
+
+            char[] arrayCaracteres = { 'A', 'r', 'r', 'a', 'y' };
+
+            var palavra = new string(arrayCaracteres);
+
+            Console.WriteLine($"Palavra: {palavra}");
+
+            Console.ReadKey();
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -7,9 +7,15 @@ namespace CursoCSharp
 {
     class Program
     {
+        /*
+         * Quando for chamar sua aplicação por linha de comando
+         * os args vão ser os parametros vão estar contidos em 
+         * args Ex. --no-verbose args.noVerbose = true
+         */
         static void Main()
         {
             CentralDeExercicios central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                // Módulo 01 - Fundamentos
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comanentários - Fundamentos", Comentarios.Executar},
                 {"Inferência - Fundamentos", Inferencia.Executar },
@@ -24,6 +30,26 @@ namespace CursoCSharp
                 {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar },
                 {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar },
                 {"Operadores Ternário - Fundamentos", OperadoresTernarios.Executar },
+
+                // Módulo 02 - Estruturas de Controle
+
+                // Módulo 03 - Classes e Métodos
+
+                // Módulo 04 - Coleçōes
+
+                {"Array - Coleçōes", Colecoes.Array.Executar }
+
+                // Módulo 05 - Orientação a Objetos
+
+                // Módulo 06 - Métodos e Funçōes
+
+                // Módulo 07 - Execçōes
+
+                // Módulo 08 - Explorando API
+
+                // Módulo 09 - Tópicos Avançados
+
+
             });
              
             central.SelecionarEExecutar();
