676c6c52b7120bbc93c7f98200c48083d654a670
Leandro <leandrodbdias@gmail.com>
2020-07-04 13:01:47 -0300

    Operador Ternário

---
+++ CursoCSharp/CursoCSharp/Fundamentos/OperadoresTernarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos
+{
+    public class OperadoresTernarios
+    {
+        public static void Executar()
+        {
+            Console.Write("Digite sua nota: ");
+
+            double.TryParse(Console.ReadLine(), out double nota);
+
+            Console.WriteLine(nota >= 7.0 ? "Aprovado" : "Reprovado");
+
+            Console.ReadLine();
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -23,6 +23,7 @@ namespace CursoCSharp
                 {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar },
                 {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar },
                 {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar },
+                {"Operadores Ternário - Fundamentos", OperadoresTernarios.Executar },
             });
              
             central.SelecionarEExecutar();
