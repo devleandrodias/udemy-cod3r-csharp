85e36b5223a69e1a278e3f259b1dc7d7b962c6c0
Leandro <leandrodbdias@gmail.com>
2020-07-06 19:40:02 -0300

    Trabalhando com métodos estáticos

---
+++ CursoCSharp/CursoCSharp/ClassesMetodos/MetodosEstatico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesMetodos
+{
+    public class CalculadoraEstatica
+    {
+        /*
+         * Quando há um método estático ele não pertence mais ao objeto e 
+         * sim a classe e não é necessário instânciar uma classe para acessa-lo.
+         * Quando um método não é estático ele só é acessivel através da
+         * instância.
+         */
+
+        public static int Somar(int x, int y) => x + y;
+        public static int Multiplicar(int x, int y) => x * y;
+    }
+
+    public class MetodosEstatico
+    {
+        public static void Executar()
+        {
+            Console.WriteLine(CalculadoraEstatica.Multiplicar(5, 10));
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -39,6 +39,7 @@ namespace CursoCSharp
                 {"Membros e Métodos - Classes e Métodos", Membros.Executar },
                 {"Construtores - Classes e Métodos", Construtores.Executar },
                 {"Métodos com Retorno - Classes e Métodos", MetodosComRetorno.Executar },
+                {"Métodos Estáticos - Classes e Métodos", MetodosEstatico.Executar },
 
                 // Módulo 04 - Coleçōes
 
