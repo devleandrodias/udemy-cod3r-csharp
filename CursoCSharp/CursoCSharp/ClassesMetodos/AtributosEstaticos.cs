09fe5860c63e68b551b7d05169dbe959edeef73f
Leandro <leandrodbdias@gmail.com>
2020-07-06 20:04:45 -0300

    Trabalhando com atributos estáticos em C#

---
+++ CursoCSharp/CursoCSharp/ClassesMetodos/AtributosEstaticos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesMetodos
+{
+    public class Produto
+    {
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, double desconto)
+        {
+            Nome = nome;
+            Preco = preco;
+            Desconto = desconto;
+        }
+
+        public string Nome;
+        public double Preco;
+        public static double Desconto = 0.1;
+
+        public double CalcularPrecoDesconto() => Preco - (Preco * Desconto);
+    }
+
+    public class AtributosEstaticos
+    {
+        public static void Executar()
+        {
+            Produto.Desconto = 0.5;
+
+            /*
+             * Atributos estáticos pertencem as classes e são usados por todos
+             */
+
+            Produto p1 = new Produto("Caneta", 3.2, 0.2);
+
+            Produto p2 = new Produto() {
+                Nome = "Lápis",
+                Preco = 1.25,
+            };
+
+            Console.WriteLine($"Produto {p1.Nome}, por {p1.Preco} reais");
+            Console.WriteLine($"Produto {p2.Nome}, por {p2.Preco} reais");
+
+            Console.ReadKey();
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -40,6 +40,7 @@ namespace CursoCSharp
                 {"Construtores - Classes e Métodos", Construtores.Executar },
                 {"Métodos com Retorno - Classes e Métodos", MetodosComRetorno.Executar },
                 {"Métodos Estáticos - Classes e Métodos", MetodosEstatico.Executar },
+                {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar },
 
                 // Módulo 04 - Coleçōes
 
