c83e6a589b756ebd5a0dc95b84ca23565ada8bb9
Leandro <leandrodbdias@gmail.com>
2020-07-06 19:28:08 -0300

    Calculadora melhora com encademento de chamadas

--- CursoCSharp/CursoCSharp/ClassesMetodos/MetodosComRetorno.cs
+++ CursoCSharp/CursoCSharp/ClassesMetodos/MetodosComRetorno.cs
@@ -16,11 +16,48 @@ namespace CursoCSharp.ClassesMetodos
         public int Somar(int a, int b, int c) => a + b + c;
     }
 
+    public class CalculadoraCadeia
+    {
+        public int Resultado { get; private set; }
+
+        public CalculadoraCadeia Somar(int x)
+        {
+            Resultado += x;
+
+            /*
+             * this = instância atual do objeto que você está trabalhando
+             */
+
+            return this;
+        }
+
+        public CalculadoraCadeia Multiplicar(int x)
+        {
+            Resultado *= x;
+            return this;
+        }
+
+        public CalculadoraCadeia Limpar()
+        {
+            Resultado = 0;
+            return this;
+        }
+
+        public CalculadoraCadeia Imprimir()
+        {
+            Console.WriteLine(Resultado);
+            return this;
+        }
+    }
+
     public class MetodosComRetorno
     {
         public static void Executar()
         {
             Console.WriteLine(new CalculadoraComum().Somar(5, 10, 5));
+
+            _ = new CalculadoraCadeia()
+                .Somar(5).Somar(15).Multiplicar(2).Multiplicar(100).Imprimir();
 
             Console.ReadKey();
         }
